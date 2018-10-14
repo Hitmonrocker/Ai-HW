7181b37000c0bc6356fcedecae70cc3513a40e16
Hitmonrocker <vinstert@comcast.net>
2018-10-13 21:50:30 -0400

    Add files via upload

--- xyz007/xyz007/mp440.py
+++ xyz007/xyz007/mp440.py
@@ -1,5 +1,5 @@
 import random
-
+import queue
 ''' ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
 '''
                 For Search Algorithms 
@@ -106,7 +106,9 @@ Compute a random state
 '''
 def get_random_state(n):
     state = []
-    # Your code here 
+    random.seed();
+    for x in range(0,n):
+    	state[x] = random.randint(0,7);
     return state
 
 '''
@@ -114,7 +116,16 @@ Compute pairs of queens in conflict
 '''
 def compute_attacking_pairs(state):
     number_attacking_pairs = 0
-    # Your code here 
+    for x in xrange(0,len(state)):
+    	col_curr = state[x]
+    	for y in xrange(x,len(state)):
+    		col_comp = state[y]
+    		if(col_curr==col_comp):
+    			number_attacking_pairs+=1
+    		if(col_curr==col_comp+x):
+    			number_attacking_pairs+=1
+    		if(col_curr==col_comp-x):
+    			number_attacking_pairs+=1
     return number_attacking_pairs
 
 '''
@@ -122,7 +133,22 @@ The basic hill-climing algorithm for n queens
 '''
 def hill_desending_n_queens(state, comp_att_pairs):
     final_state = []
-    # Your code here
+    new_state = state;
+    num_atk = 0;
+    while(new_state != final_state):
+    	
+    	num_atk = comp_att_pairs(new_state)
+
+		if(num_atk == 0)
+    		return final_state;
+
+		for x in xrange(len(state),0,-1):
+			if(comp_att_pairs(state[x]+1)<num_atk && (state[x]+1)<7):
+				state[x]+=1;
+	    	if(comp_att_pairs(state[x]-1)<num_atk && (state[x]-1)>0):
+	    		state[x]-=1;
+	    final_state = new_state;
+	    new_state = state;
     return final_state
 
 '''
@@ -130,7 +156,13 @@ Hill-climing algorithm for n queens with restart
 '''
 def n_queens(n, get_rand_st, comp_att_pairs, hill_descending):
     final_state = []
-    # Your code here
+    old_state = get_rand_st(n);
+    final_state = old_state;
+   	while(comp_att_pairs(final_state)!=0):
+   		old_state = final_state;
+   		final_state = hill_descending(start_state,comp_att_pairs);
+   		if((final_state == old_state).all()):
+   			final_state = get_rand_st(n);
     return final_state
 
 
